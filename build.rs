326e8656099679f1612bc5002f90e553986287db
amorf <fatihermetin@gmail.com>
2023-07-12 20:23:27 +0300

    Add tonic build

--- build.rs
+++ build.rs
@@ -1,3 +1,12 @@
 fn main() {
+    let protos_path = "proto";
+    let file_descriptor_path = "blabla";
+
     //Proto files will be compiled here & also reflection feature will be initialized here
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(false)
+        .file_descriptor_set_path("ReflectionFilePath")
+        .compile(&["proto/user/v1/user.proto"], &[protos_path])
+        .expect("Error on proto files build");
 }
