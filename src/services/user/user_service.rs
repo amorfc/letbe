b0587c115c16dd0662a33fc0462d7c570d1a9a03
amorf <fatihermetin@gmail.com>
2023-07-20 01:47:03 +0300

    Validate register user request on request

--- src/services/user/user_service.rs
+++ src/services/user/user_service.rs
@@ -1,10 +1,16 @@
 use tonic::{Request, Response, Status};
 
-use crate::services::proto::user::{
-    user_server::User, RegisterUserRequest, RegisterUserResponse, RegisteredUserResponseData,
+use crate::services::{
+    common::request::request_validator::RequestValidator,
+    proto::user::{
+        user_server::User, RegisterUserRequest, RegisterUserResponse, RegisteredUserResponseData,
+    },
+    user::user_request::RequestUser,
 };
 
-pub struct UserService;
+pub struct UserService {
+    // user_repository: Box<dyn UserRepository>,
+}
 
 #[tonic::async_trait]
 impl User for UserService {
@@ -13,6 +19,17 @@ impl User for UserService {
         request: Request<RegisterUserRequest>,
     ) -> Result<Response<RegisterUserResponse>, Status> {
         dbg!(&request);
+
+        let user = request.into_inner();
+        let request_user = RequestUser::from(user);
+
+        RequestValidator::new(request_user).validate_for_response()?;
+
+        // let user = User::new(user.name, user.email, user.password);
+        // let user = self.user_repository.save(user).await?;
+        // let user = RegisteredUserResponseData::from(user);
+        // let response = RegisterUserResponse::new(user);
+        // Ok(Response::new(response))
 
         Ok(Response::new(RegisterUserResponse {
             data: Some(RegisteredUserResponseData {
