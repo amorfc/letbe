63658ba597367fb749dcdf2a3b8ec5f416c38c0d
amorf <fatihermetin@gmail.com>
2023-07-30 17:29:14 +0300

    Imp user manage and user registration method

--- src/application/managers/user/user_manager.rs
+++ src/application/managers/user/user_manager.rs
@@ -1,13 +1,41 @@
+use crate::{
+    application::{
+        domain::models::user::domain_user_model::DomainUserModel,
+        repositories::user::user_repository::UserRepositoryTrait,
+    },
+    infra::db_initializor::LetDbConnection,
+    services::user::user_request::NewUserActiveModelWrapper,
+};
+
 #[tonic::async_trait]
 pub trait UserManagerTrait {
-    fn new() -> Self;
-    async fn user_registration(&self) {}
+    fn new(db_connection: LetDbConnection) -> Self;
+    async fn user_registration<T: Into<NewUserActiveModelWrapper> + Send>(
+        &self,
+        new_user: T,
+    ) -> Result<DomainUserModel, String>;
 }
 
-pub struct UserManager {}
+pub struct UserManager<T: UserRepositoryTrait> {
+    user_repo: T,
+}
 
-impl UserManagerTrait for UserManager {
-    fn new() -> Self {
-        Self {}
+#[tonic::async_trait]
+impl<R: UserRepositoryTrait + Send + Sync> UserManagerTrait for UserManager<R> {
+    fn new(db_conneciton: LetDbConnection) -> Self {
+        let user_repo = R::new(db_conneciton);
+        Self { user_repo }
+    }
+    async fn user_registration<T: Into<NewUserActiveModelWrapper> + Send>(
+        &self,
+        new_user: T,
+    ) -> Result<DomainUserModel, String> {
+        let NewUserActiveModelWrapper(active_model) = new_user.into();
+
+        let registered_user = self.user_repo.create_user(active_model).await?;
+
+        let domain_user_model = DomainUserModel::from(registered_user);
+
+        Ok(domain_user_model)
     }
 }
