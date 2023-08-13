7124ca34dec293394e882e44c3290cac7c4900a2
amorf <fatihermetin@gmail.com>
2023-08-14 01:46:35 +0300

    Move db providers

--- src/application/repositories/common/repository.rs
+++ src/application/repositories/common/repository.rs
@@ -4,18 +4,6 @@ use sea_orm::{
 
 use crate::infra::db_initializor::{LetDbConnection, LetDbTransaction};
 
-pub trait DbConnectionProvider {
-    fn db_connection(&self) -> &LetDbConnection;
-}
-
-// Define a new trait that provides the db_transaction method
-#[tonic::async_trait]
-pub trait DbTransactionProvider: DbConnectionProvider {
-    async fn db_tx(&self) -> Result<LetDbTransaction, DbErr> {
-        self.db_connection().begin().await
-    }
-}
-
 #[tonic::async_trait]
 pub trait RepositoryTrait<A, E>: DbConnectionProvider
 where
@@ -39,5 +27,17 @@ where
             Ok(model) => Ok(model.into_active_model()),
             Err(_) => return Err("Failed to insert model".to_string()),
         }
+    }
+}
+
+pub trait DbConnectionProvider {
+    fn db_connection(&self) -> &LetDbConnection;
+}
+
+// Define a new trait that provides the db_transaction method
+#[tonic::async_trait]
+pub trait DbTransactionProvider: DbConnectionProvider {
+    async fn db_tx(&self) -> Result<LetDbTransaction, DbErr> {
+        self.db_connection().begin().await
     }
 }
