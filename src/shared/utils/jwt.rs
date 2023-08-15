a68d1b35ba8e39342c289696929d2a4547588a60
amorf <fatihermetin@gmail.com>
2023-08-15 19:30:14 +0300

    Add jwt struct white box tests

--- src/shared/utils/jwt.rs
+++ src/shared/utils/jwt.rs
@@ -71,3 +71,73 @@ impl LettJwt {
         ENV_CONFIG.jwt_secret.clone()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{env, thread, time::Duration};
+
+    use super::*;
+    fn load_dummy_envs() {
+        env::set_var("DB", "test_db");
+        env::set_var("DB_PORT", "5432");
+        env::set_var("HOST", "localhost");
+        env::set_var("HOST_PORT", "8080");
+        env::set_var("POSTGRES_USER", "test_user");
+        env::set_var("POSTGRES_PASSWORD", "test_password");
+        env::set_var("SECRET", "test_secret");
+        env::set_var("ENVIRONMENT", "test");
+    }
+
+    #[test]
+
+    fn valid_jwt_tests() {
+        load_dummy_envs();
+        let user_id = 31;
+        let device_id = "AppleMacbook123123".to_string();
+        let claims = LettJwtClaims::new(user_id, device_id, None);
+
+        let token = LettJwt::encode(&claims).unwrap();
+
+        let decoded_claims = LettJwt::decode(&token).unwrap();
+
+        assert_eq!(claims, decoded_claims);
+    }
+
+    #[test]
+    fn invalid_jwt_tests() {
+        load_dummy_envs();
+        let user_id = 31;
+        let device_id = "AppleMacbook123123".to_string();
+        let mut claims = LettJwtClaims::new(user_id, device_id, None);
+
+        let token = LettJwt::encode(&claims).unwrap();
+
+        claims.user_id = 0;
+
+        let decoded_claims = LettJwt::decode(&token).unwrap();
+
+        assert_ne!(claims, decoded_claims);
+    }
+
+    #[test]
+    fn expired_jwt_tests() {
+        load_dummy_envs();
+
+        let one_sec_dur = Duration::from_secs(1);
+        let expr_millis = one_sec_dur.as_millis() as usize;
+        let user_id = 31;
+        let device_id = "AppleMacbook123123".to_string();
+        let claims = LettJwtClaims::new(user_id, device_id, Some(expr_millis));
+
+        let token = LettJwt::encode(&claims).unwrap();
+
+        thread::sleep(one_sec_dur);
+
+        let rs = LettJwt::decode(&token);
+        let is_error = rs.is_err();
+        let error_message = rs.unwrap_err();
+
+        assert!(is_error);
+        assert_eq!(error_message, "ExpiredSignature".to_string());
+    }
+}
