74138e6e826a772e3751da6891a38be1bcee8c81
amorf <fatihermetin@gmail.com>
2023-08-16 00:15:55 +0300

    Add db datetime creator utils

--- src/shared/utils/datetime.rs
+++ src/shared/utils/datetime.rs
@@ -1,7 +1,24 @@
+use chrono::{DateTime, NaiveDateTime, Utc};
+use sea_orm::prelude::DateTimeWithTimeZone;
+
 pub struct LettDate {}
 
 impl LettDate {
     pub fn utc_now() -> chrono::DateTime<chrono::Utc> {
         chrono::Utc::now()
+    }
+
+    pub fn dt_with_tz(ts: usize) -> Result<DateTimeWithTimeZone, String> {
+        let native_date_time =
+            NaiveDateTime::from_timestamp_millis(ts as i64).ok_or("Invalid timestamp")?;
+        let date_time_utc: DateTime<Utc> = DateTime::from_utc(native_date_time, Utc);
+
+        Ok(DateTimeWithTimeZone::from(date_time_utc))
+    }
+
+    pub fn now_dt_with_tz() -> DateTimeWithTimeZone {
+        let date_time_utc: DateTime<Utc> = Self::utc_now();
+
+        DateTimeWithTimeZone::from(date_time_utc)
     }
 }
