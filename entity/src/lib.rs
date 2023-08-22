1e8d97e514938a9e1cc75a3498f492a7e3697b05
amorf <fatihermetin@gmail.com>
2023-08-22 15:59:29 +0300

    Add needed props for entities

--- entity/src/lib.rs
+++ entity/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod prelude;
 
 pub mod authn;
+pub mod sea_orm_active_enums;
 pub mod user;
--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,9 +1,8 @@
 pub use entity::prelude::*;
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_user_table;
-mod m20220101_000002_create_enum;
 mod m20230814_192414_create_authn_table;
+mod m20230822_121559_create_user_table;
 pub struct Migrator;
 
 pub mod utils;
@@ -12,8 +11,7 @@ pub mod utils;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000002_create_enum::Migration),
-            Box::new(m20220101_000001_create_user_table::Migration),
+            Box::new(m20230822_121559_create_user_table::Migration),
             Box::new(m20230814_192414_create_authn_table::Migration),
         ]
     }
