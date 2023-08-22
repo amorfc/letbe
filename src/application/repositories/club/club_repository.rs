7220e1c145f67f615c8a94e1887c5ea9de41b8a6
amorf <fatihermetin@gmail.com>
2023-08-22 19:46:34 +0300

    Add club repository

---
+++ src/application/repositories/club/club_repository.rs
@@ -0,0 +1,40 @@
+use anyhow::Result;
+use entity::club as ClubEntity;
+use sea_orm::entity::prelude::*;
+
+use crate::{
+    application::repositories::common::repository::{DbConnectionProvider, RepositoryTrait},
+    infra::db_initializor::LetDbConnection,
+};
+
+// User Manager Trait that requires UserRepositoryTrait
+#[tonic::async_trait]
+pub trait ClubRepositoryTrait:
+    RepositoryTrait<ClubEntity::ActiveModel, ClubEntity::Entity>
+{
+    async fn create_club(&self, user: ClubEntity::ActiveModel) -> Result<ClubEntity::ActiveModel> {
+        let a = self.save(user).await?;
+        Ok(a)
+    }
+}
+
+// Implementation of UserRepositoryTrait
+pub struct ClubRepositoryImpl {
+    db_connection: LetDbConnection,
+}
+
+#[tonic::async_trait]
+impl ClubRepositoryTrait for ClubRepositoryImpl {}
+
+impl ClubRepositoryImpl {
+    pub fn new(db_connection: LetDbConnection) -> Self {
+        Self { db_connection }
+    }
+}
+
+impl DbConnectionProvider for ClubRepositoryImpl {
+    fn db_connection(&self) -> &DatabaseConnection {
+        &self.db_connection
+    }
+}
+impl RepositoryTrait<ClubEntity::ActiveModel, ClubEntity::Entity> for ClubRepositoryImpl {}
