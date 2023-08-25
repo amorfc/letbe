c85efcc9a5333e8cd9712497d27ed2d9ef763b7d
amorf <fatihermetin@gmail.com>
2023-08-25 13:57:40 +0300

    Add manager club creation flow

--- src/application/managers/club/club_manager.rs
+++ src/application/managers/club/club_manager.rs
@@ -1,19 +1,22 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 
 use crate::{
     application::{
         domain::models::club::domain_club_model::DomainClubModel,
         managers::common::manager::ManagerTrait,
-        repositories::club::club_repository::ClubRepositoryImpl,
+        repositories::club::club_repository::{ClubRepositoryImpl, ClubRepositoryTrait},
     },
     infra::db_initializor::LetDbConnection,
-    services::common::response::response_status::LettResError,
-    shared::utils::datetime::LettDate,
+    services::{
+        club::creation::request::{NewClub, NewClubActiveModelWrapper},
+        common::response::response_status::LettResError,
+    },
 };
 
 #[tonic::async_trait]
 pub trait ClubManagerTrait: ManagerTrait<DomainClubModel> {
-    async fn club_creation(&self) -> Result<DomainClubModel, LettResError>;
+    async fn club_creation(&self, new_club: NewClub) -> Result<DomainClubModel, LettResError>;
+    async fn check_club_name_availability(&self, name: &str) -> Result<()>;
 }
 
 // Implementation of UserManagerTrait
@@ -31,16 +34,24 @@ impl ClubManagerImpl {
 
 #[tonic::async_trait]
 impl ClubManagerTrait for ClubManagerImpl {
-    async fn club_creation(&self) -> Result<DomainClubModel, LettResError> {
-        let club_domain_model = DomainClubModel {
-            id: 0,
-            name: "test".to_string(),
-            legal_name: "test".to_string(),
-            created_at: LettDate::now_dt_with_tz(),
-            updated_at: None,
-            deleted_at: None,
-        };
+    async fn club_creation(&self, new_club: NewClub) -> Result<DomainClubModel, LettResError> {
+        self.check_club_name_availability(&new_club.name).await?;
+
+        let new_club_active_model: NewClubActiveModelWrapper = new_club.into();
+
+        let res = self.repo.create_club(new_club_active_model.0).await?;
+
+        let club_domain_model = DomainClubModel::from(res);
         Ok(club_domain_model)
+    }
+
+    async fn check_club_name_availability(&self, name: &str) -> Result<()> {
+        let find_club = self.repo.find_club_by_name(name).await?;
+        if let Some(exists_club) = find_club {
+            bail!("Club with name {} already exists", exists_club.name);
+        }
+
+        Ok(())
     }
 }
 
